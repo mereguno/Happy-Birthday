815939144142b3b6b2ac6541702bde13540714a8
Brack <heraldbrack@gmail.com>
2021-09-12 17:11:57 -0400

    First push for credit.

---
+++ app/src/main/java/com/example/HappyBirthday/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.HappyBirthday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.*;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.e("MainActivity", "Error");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
